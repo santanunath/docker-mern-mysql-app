2809a649a594c59a121140e27684df641cfbcf61
santanunath <43264004+santanunath@users.noreply.github.com>
2025-06-30 15:26:13 +0530

    Update Books.jsx

--- frontend/src/pages/Books.jsx
+++ frontend/src/pages/Books.jsx
@@ -3,7 +3,7 @@ import {useEffect, useState} from 'react'
 import axios from 'axios'
 import { Link } from "react-router-dom";
 
-const BACKEND_API_URL = "http://backend:4000";
+const BACKEND_API_URL = "http://backend:4000"; // defined in docker-compose.yml
 
 const Books=()=> {
   const [books, setBooks]= useState([]);
