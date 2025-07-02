6c86f1737e48483880722d6fd9d500b66afed99b
santanunath <43264004+santanunath@users.noreply.github.com>
2025-07-03 04:18:00 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,8 +14,11 @@ import "./style.css"
 
 
 function App() {
+
   return (
+
     <div className="App">
+
       <BrowserRouter>
         <Routes>
           <Route path="/"           element={<Books/>}/>
@@ -23,6 +26,7 @@ function App() {
           <Route path='/update/:id' element={<Update/>}/>
         </Routes>
       </BrowserRouter>
+
     </div>
   );
 }
