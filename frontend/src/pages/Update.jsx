c879d0a969b72ce0ef25871df6f3ed9c53686ebf
santanunath <43264004+santanunath@users.noreply.github.com>
2025-07-03 04:13:23 +0530

    Update Update.jsx

--- frontend/src/pages/Update.jsx
+++ frontend/src/pages/Update.jsx
@@ -6,11 +6,10 @@ import {useLocation, useNavigate} from "react-router-dom"
 const BACKEND_API_URL = "http://backend:4000"; // defined in docker-compose.yml
 
 const Update=()=> {
+
   const [book,setBook]=useState({
     title:"",
     desc:"",
-    price:null,
-    cover:"",
   });
 
   const navigate=useNavigate()
@@ -21,6 +20,7 @@ const Update=()=> {
   const handleChange=(e)=>{
     setBook((prev)=>({ ...prev, [e.target.name]:e.target.value}));
   };
+
   console.log(book)
   
   const handleClick =async e=>{
@@ -40,6 +40,7 @@ const Update=()=> {
   return (
 
     <div className='form'>
+
       <h1>Update the Book</h1>
 
       <input
@@ -54,20 +55,6 @@ const Update=()=> {
       placeholder='Description'
       onChange={handleChange}
       name='desc'
-      />
-
-      <input
-      type='number'
-      placeholder='Price'
-      onChange={handleChange}
-      name='price'
-      />
-
-      <input
-      type='text'
-      placeholder='Cover Image'
-      onChange={handleChange}
-      name='cover'
       />
       
       <button className='formButton' onClick={handleClick}>Add</button>
